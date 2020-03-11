4ec4d7971edaf1310f7f97a923e85a410cd8ac71
[jhao0413] <[384716045@qq.com]>
2020-03-11 19:33:37 +0800

    ES6写法

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,12 @@
-module.exports = {
-    devServer: {
-      proxy: {
-        '/api': {
-          target: 'https://u.y.qq.com', //对应自己的接口
-          changeOrigin: true, //跨域为true
-          ws: true,
-          pathRewrite: {
-            '^/api': ''//自定义地址名
-          }
-        }
+export const devServer = {
+  proxy: {
+    '/api': {
+      target: 'https://u.y.qq.com',
+      changeOrigin: true,
+      ws: true,
+      pathRewrite: {
+        '^/api': '' //自定义地址名
       }
     }
   }
+};
